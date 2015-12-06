eed7ebd864ffa8aab71fa2daa9303d5ae099fd1c
rowland <rowlandekemezie@gmail.com>
2015-12-06 12:48:15 +0100

    feat(controllers): update document controller
    -add CRUD methods to document.controller.js
    -add comments to user.controller.js

--- app/controllers/document.controller.js
+++ app/controllers/document.controller.js
@@ -1,27 +1,169 @@
 var models = require('./../models');
-
 var User = models.User;
 var Role = models.Role;
 var Document = models.Document;
-
 module.exports = {
-
- // method to create document
-createDocument : function(req, res){
- Role.findOne({title: res.body.role}, function(err, role){
-  if(err){
-   res.send(err);
-  }else {
-   if(!role){
-    res.json({
-     succes: false,
-     message: 'invalid role'
+  /**
+   * [createDocument method]
+   * @param  {[JSON]} req [http request body]
+   * @param  {[JSON]} res [response on request]
+   * @return {[JSON]}     [status]
+   */
+  createDocument: function(req, res) {
+    var doc = req.body;
+    Role.findOne({
+      title: doc.role
+    }, function(err, role) {
+      if (err) {
+        res.send(err);
+      } else {
+        if (!role) {
+          res.status(401).json({
+            succes: false,
+            message: 'Invalid role'
+          });
+        } else if (role) {
+          Document.findOne({
+            title: doc.title
+          }, function(err, docs) {
+            if (docs) {
+              res.status(403).json({
+                succes: false,
+                message: 'Document exist'
+              });
+            } else if (!docs) {
+              var userId = req.decoded._id;
+              req.body.ownerId = userId;
+              var newDoc = new Document(doc);
+              newDoc.save(function(err) {
+                if (err) {
+                  res.send(err);
+                } else {
+                  res.status(200).json({
+                    success: true,
+                    message: 'Document successfully created'
+                  });
+                }
+              });
+            }
+          });
+        }
+      }
     });
-   }else if(role){
-    User.findOne({})
-   }
-  }
- })
-};
+  },
+  /**
+   * [getAllDocuments method]
+   * @param  {[JSON]} req [http request to get endpoint]
+   * @param  {[JSON]} res [response on request]
+   * @return {[JSON]}     [Status and json documents on success]
+   */
+  getAllDocuments: function(req, res) {
+    Document.find({}).sort({
+      dateCreated: -1
+    }).limit(req.params.limit).exec(function(err, docs) {
+      if (err) {
+        res.send(err);
+      } else if (!docs) {
+        res.send({
+          success: false,
+          message: 'No document found'
+        });
+      } else {
+        res.json(docs);
+      }
+    });
+  },
+  /**
+   * [getDocumentById method]
+   * @param  {[JSON]} req [request params]
+   * @param  {[JSON]} res [response on request]
+   * @return {[JSON]}     [status]
+   */
+  getDocumentById: function(req, res) {
+    Document.findOne({
+      _id: req.params.id
+    }, function(err, doc) {
+      if (err) {
+        res.send(err);
+      } else if (!doc) {
+        res.send({
+          success: false,
+          message: 'No document found for the Id'
+        });
+      } else {
+        res.json(doc);
+      }
+    });
+  },
+  /**
+   * [updateDocument method]
+   * @param  {[JSON]} req [request params]
+   * @param  {[JSON]} res [response on request]
+   * @return {[JSON]}     [status of search result]
+   */
+  updateDocument: function(req, res) {
 
+    Document.findByIdAndUpdate(req.params.id, req.body, function(err, doc) {
+      if (err) {
+        res.send(err);
+      } else if (!doc) {
+        res.status(404).json({
+          success: false,
+          message: 'Document not available'
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Document updated successfully'
+        });
+      }
+    });
+  },
+
+  /**
+   * [deleteDocument method]
+   * @param  {[JSON]} req [request params]
+   * @param  {[JSON]} res [response on request]
+   * @return {[JSON]}     [status]
+   */
+  deleteDocument: function(req, res) {
+    Document.findByIdAndRemove(req.params.id, req.body, function(err, doc) {
+      if (err) {
+        res.send(err);
+      } else if (!doc) {
+        res.status(404).json({
+          success: false,
+          message: 'Document not available'
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Document deleted successfully'
+        });
+      }
+    });
+  },
+
+  /**
+   * [getAllDocumentsForUser method]
+   * @param  {[JSON]} req [http request params]
+   * @param  {[JSON]} res [http response on response]
+   * @return {[JSON]}     [json status of the request]
+   */
+  getAllDocumentsForUser: function(req, res) {
+    Document.find({
+      ownerId: req.params.id
+    }, function(err, docs) {
+      if (err) {
+        res.send(err);
+      } else if (!docs) {
+        res.status(404).json({
+          success: false,
+          message: 'User have no document'
+        });
+      } else {
+        res.status(200).json(docs);
+      }
+    });
+  }
 };
--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,211 +1,259 @@
-var db = require('./../config/database');
 var models = require('./../models');
 var config = require('./../config/pass');
 var jwt = require('jsonwebtoken');
 
-// instantiate
+/**
+ * Models instancies
+ * @type {[Objects]}
+ */
 var User = models.User;
 var Role = models.Role;
 var Document = models.Document;
 
-
 module.exports = {
 
- // Authentication middleware to protect routes
- Auth: function(req, res, next) {
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  if (!token) {
-   res.status(403).send({
-    sucsess: false,
-    message: "please provide your token"
-   });
-   return;
-  } else {
-   jwt.verify(token, config.secret, function(err, decoded) {
-    if (err) {
-     res.status(401).json({
-      success: false,
-      message: "Authentication failed"
-     });
+  /**
+   * [Authenthication middleware to protect certain routes]
+   * @param {[http request]}   req  [description]
+   * @param {[http response]}   res  [description]
+   * @param {Function} next [description]
+   */
+  Auth: function(req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (!token) {
+      res.status(403).send({
+        sucsess: false,
+        message: "Please provide your token"
+      });
+      return;
     } else {
-     req.decoded = decoded;
-     next();
-    }
-   });
-  }
- },
-
- // for user logOut
- logOut: function(req, res) {
-  req.session.destroy(function(err) {
-   if (err) {
-    res.send(err);
-   } else {
-    res.json({
-     success: true,
-     message: "You are logged out"
-    });
-   }
-  });
- },
-
- // user sign in method
- signIn: function(req, res) {
-  User.findOne({
-   userName: req.body.userName
-  }, function(err, user) {
-   if (err) {
-    throw err;
-   }
-   if (!user) {
-    res.status(401).send({
-     success: false,
-     message: "Invalid user"
-    });
-   } else if (user) {
-    if (req.body.password !== user.password) {
-     res.json({
-      success: false,
-      message: 'Invalid password'
-     });
-    } else {
-     var token = jwt.sign(user, config.secret, {
-      expiresInMinutes: 1440 // expires in 24 hrs
-     });
-     res.json({
-      token: token,
-      success: true,
-      message: "Login successful"
-     });
-    }
-   }
-  });
- },
- // create User method
- createUser: function(req, res) {
-  if (!req.body.role) {
-   res.status(401).json({
-    success: false,
-    message: 'please, provide your role'
-   });
-  } else {
-   Role.findOne({
-    title: req.body.role
-   }, function(role) {
-    console.log(role);
-    if (!role) {
-     res.status(401).json({
-      success: false,
-      message: 'Invalid role'
-     });
-     return;
-    } else {
-     User.findOne({
-      userName: req.body.userName
-     }, function(err, user) {
-      if (err) {
-       res.send(err);
-
-      } else if (user) {
-       res.status(401).send({
-        sucess: false,
-        message: 'userName already exists'
-       });
-
-
-      } else if (!user) {
-       if (!req.body.firstName && !req.body.lastName) {
-        res.status(401).send({
-         success: false,
-         message: 'please provide your firstName and lastName'
-        });
-
-       } else if (req.body.password.length < 8 || undefined) {
-        res.status(403).send({
-         success: false,
-         message: 'password must not be less than 8 characters'
-        });
-
-
-       } else if (!req.body.email) {
-        res.status(403).send({
-         success: false,
-         message: 'please enter your email'
-        });
-       }
-
-      } else {
-       var newUser = new User(req.body);
-       newUser.save(function(err) {
+      jwt.verify(token, config.secret, function(err, decoded) {
         if (err) {
-         res.send(err);
+          res.status(401).json({
+            success: false,
+            message: "Authentication failed"
+          });
         } else {
-         res.status(200).json({
-          success: true,
-          message: 'created successfully'
-         });
+          req.decoded = decoded;
+          next();
         }
-       });
-      }
-     });
+      });
     }
-   } );
-  }
- },
+  },
 
- getAllUsers: function(req, res) {
-  User.find({}, function(err, users) {
-   if (err) {
-    res.send(err);
-   } else if (!users) {
-    res.status(404).send({
-     success: false,
-     message: 'No user found'
+  /**
+   * [logOut method]
+   * @param  {[type]} req [description]
+   * @param  {[type]} res [description]
+   * @return {[type]}     [description]
+   */
+  logOut: function(req, res) {
+    req.session.destroy(function(err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({
+          success: true,
+          message: "You are logged out"
+        });
+      }
     });
-   } else {
-    res.json(users);
-   }
-  });
- },
+  },
 
- getUser: function(req, res) {
-  User.findById(req.params.id, function(err, user) {
-   if (err) {
-    res.send(err);
-   } else if (!user) {
-    res.status(404).send({
-     success: false,
-     message: 'No user found by that Id'
+  /**
+   * [signIn method]
+   * @param  {[type]} req [description]
+   * @param  {[type]} res [description]
+   * @return {[type]}     [description]
+   */
+  signIn: function(req, res) {
+    User.findOne({
+      userName: req.body.userName
+    }, function(err, user) {
+      if (err) {
+        throw err;
+      }
+      if (!user) {
+        res.status(401).send({
+          success: false,
+          message: "Invalid user"
+        });
+      } else if (user) {
+        var isValid = user.comparePassword(req.body.password);
+        if (!isValid) {
+          res.json({
+            success: false,
+            message: 'Invalid password'
+          });
+        } else {
+          var token = jwt.sign(user, config.secret, {
+            expiresInMinutes: 1440 // expires in 24 hrs
+          });
+          res.json({
+            token: token,
+            success: true,
+            message: "Login successful"
+          });
+        }
+      }
     });
-   } else {
-    res.json(user);
-   }
-  });
- },
+  },
 
- updateUser: function(req, res) {
-  User.findByIdAndUpdate(req.params.id, req.body, function(err, user) {
-   if (err) {
-    res.send(err);
-   } else {
-    res.json({
-     success: true,
-     message: 'User details updated'
-    });
-   }
-  });
- },
+  /**
+   * [createUser method]
+   * @param  {[type]} req [description]
+   * @param  {[type]} res [description]
+   * @return {[type]}     [description]
+   */
+  createUser: function(req, res) {
+    var userData = req.body;
+    console.log(userData.role);
+    if (!userData.role) {
+      res.status(401).json({
+        success: false,
+        message: 'Please, provide your role'
+      });
+    } else if (!(userData.firstName && userData.lastName)) {
+      res.status(401).json({
+        success: false,
+        message: 'Please, provide your firstName and lastName'
+      });
+    } else if ((userData.password).length < 8 || undefined) {
+      res.status(403).send({
+        success: false,
+        message: 'Password must not be less than 8 characters'
+      });
+    } else if (!userData.email || !userData.userName) {
+      res.status(403).send({
+        success: false,
+        message: 'Please enter your userName and email'
+      });
+    } else {
+      Role.findOne({
+        title: userData.role
+      }, function(err, roles) {
+        if (!roles) {
+          res.status(401).send({
+            success: false,
+            message: 'Invalid role'
+          });
+        } else {
+          User.findOne({
+            userName: userData.userName
+          }, function(err, users) {
+            if (users) {
+              res.status(401).send({
+                success: false,
+                message: 'UserName already exists'
+              });
+            } else {
+              var newUser = new User(userData);
+              newUser.save(function(err) {
+                if (err) {
+                  res.send(err);
+                } else {
+                  res.status(200).json({
+                    success: true,
+                    message: 'User created successfully'
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  },
 
- deleteUser: function(req, res) {
-  User.findByIdAndRemove(req.params.id, function(err) {
-   if (err) {
-    res.send(err);
-   } else {
-    res.json({
-     success: true,
-     message: 'User deleted successfully'
+  /**
+   * [getAllUsers method]
+   * @param  {[type]} req [description]
+   * @param  {[type]} res [description]
+   * @return {[type]}     [description]
+   */
+  getAllUsers: function(req, res) {
+    User.find({}, function(err, users) {
+      if (err) {
+        res.send(err);
+      } else if (!users) {
+        res.status(404).send({
+          success: false,
+          message: 'No user found'
+        });
+      } else {
+        res.json(users);
+      }
     });
-   }
-  });
- }
+  },
+
+  /**
+   * [getUser method]
+   * @param  {[type]} req [description]
+   * @param  {[type]} res [description]
+   * @return {[type]}     [description]
+   */
+  getUser: function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+      if (err) {
+        res.send(err);
+      } else if (!user) {
+        res.status(404).send({
+          success: false,
+          message: 'No user found by that Id'
+        });
+      } else {
+        res.json(user);
+      }
+    });
+  },
+
+  /**
+   * [updateUser method]
+   * @param  {[type]} req [description]
+   * @param  {[type]} res [description]
+   * @return {[type]}     [description]
+   */
+  updateUser: function(req, res) {
+
+    User.findByIdAndUpdate(req.params.id, req.body, function(err, user) {
+      if (err) {
+        res.send(err);
+      } else if (!user) {
+        res.status(404).json({
+          success: false,
+          message: 'User not available'
+        });
+      } else {
+        res.json({
+          success: true,
+          message: 'User details updated'
+        });
+      }
+    });
+  },
+
+  /**
+   * [deleteUser method]
+   * @param  {[type]} req [description]
+   * @param  {[type]} res [description]
+   * @return {[type]}     [description]
+   */
+  deleteUser: function(req, res) {
+
+    User.findByIdAndRemove(req.params.id, function(err, user) {
+      if (err) {
+        res.send(err);
+      } else if (!user) {
+        res.status(404).json({
+          success: false,
+          message: 'User not available'
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'User deleted successfully'
+        });
+      }
+    });
+  },
 };
