e8ebbd96d50c2963d8429ac6e264fafbae0b966b
rowland <rowlandekemezie@gmail.com>
2015-12-13 04:08:39 +0100

    chore(configuration): add body parser format for json api

--- config/express.js
+++ config/express.js
@@ -13,12 +13,13 @@ var routes = require('./../app/routes');
 var router = express.Router();
 routes(router);
 
+// instantiate an object instance of express
 var app = express();
 
 // connect to the database
 mongoose.connect(database.db);
 
-
+app.use(bodyParser.json());
 app.use(session({
   secret: 'I love andela',
   resave: false,
@@ -26,13 +27,16 @@ app.use(session({
 }));
 
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 
 // mount the router on the root directory
-app.use('/', router);
+app.get('/', function(req, res){
+	res.send('I am here');
+});
+app.use('/api', router);
 
 
 module.exports = app;
