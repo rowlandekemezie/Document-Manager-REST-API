4f9e29b54c9f644064e23c308f18379edc773a9c
andela-rekemezie <rowland.ekemezie@andela.com>
2015-12-15 10:49:04 +0100

    refactor(models): modify createdAt field
    -require dateHelper.js
    -modify pre save section

--- app/models/document.model.js
+++ app/models/document.model.js
@@ -3,8 +3,9 @@
 var mongoose = require('mongoose');
 var Role = require('./role.model');
 var User = require('./user.model');
+var date = require('./../../seeds/dateHelper')();
 
-//var Schema = mongoose.Shema;
+// var Schema = mongoose.Shema;
 var documentSchema = new mongoose.Schema({
   ownerId: {
     type: mongoose.Schema.Types.ObjectId,
@@ -28,37 +29,21 @@ var documentSchema = new mongoose.Schema({
     Ref: 'Role'
   },
 
-  dateCreated: {
-    type: Date
+  createdAt: {
+    type: String
   },
 
   lastModified: {
-    type: Date
+    type: Date,
+    default: Date.now
   }
 });
 
 //set dateCreated and lastmodified to current date before saving a document
-documentSchema.pre("save", function(next) {
+documentSchema.pre('save', function(next) {
 
   var doc = this;
-  var datetime = new Date();
-
-  // format the output
-  var month = datetime.getMonth() + 1;
-  var day = datetime.getDate();
-  var year = datetime.getFullYear();
-  var hour = datetime.getHours();
-  if (hour < 10) hour = "0" + hour;
-  var min = datetime.getMinutes();
-  if (min < 10) min = "0" + min;
-  var sec = datetime.getSeconds();
-  if (sec < 10) sec = "0" + sec;
-
-  // put it all together
-  var dateTimeString = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec;
-  doc.dateCreated = dateTimeString;
-  doc.lastModified = dateTimeString;
-
+  doc.createdAt = date;
   next();
 });
 
