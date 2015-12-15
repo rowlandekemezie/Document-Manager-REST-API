7ac6fae1615e467a24c266ea73a5c10356ed2e61
andela-rekemezie <rowland.ekemezie@andela.com>
2015-12-15 07:50:17 +0100

    feat(seed-data): add date helper method

---
+++ seeds/dateHelper.js
@@ -0,0 +1,21 @@
+ "use strict";
+
+ var date = function() {
+ var datetime = new Date();
+
+  // format the output
+  var month = datetime.getMonth() + 1;
+  var day = datetime.getDate();
+  var year = datetime.getFullYear();
+  var hour = datetime.getHours();
+  if (hour < 10) hour = "0" + hour;
+  var min = datetime.getMinutes();
+  if (min < 10) min = "0" + min;
+  var sec = datetime.getSeconds();
+  if (sec < 10) sec = "0" + sec;
+
+  // put it all together
+  var dateTimeString = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec;
+  return  dateTimeString;
+};
+module.exports =date;
