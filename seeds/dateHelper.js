f8817743b10a4186aa42b65302f7d02a0decd3cb
andela-rekemezie <rowland.ekemezie@andela.com>
2015-12-17 01:28:07 +0100

    refactor(date-helper): format file to conform with jshint configuration

--- seeds/dateHelper.js
+++ seeds/dateHelper.js
@@ -1,24 +1,30 @@
-(function (){
+(function() {
  "use strict";
 
  var date = function() {
- var datetime = new Date();
+	var datetime = new Date();
 
-  // format the output
-  var month = datetime.getMonth() + 1;
-  var day = datetime.getDate();
-  var year = datetime.getFullYear();
-  var hour = datetime.getHours();
-  if (hour < 10) {hour = "0" + hour;}
-  var min = datetime.getMinutes();
-  if (min < 10) {min = "0" + min;}
-  var sec = datetime.getSeconds();
-  if (sec < 10) {sec = "0" + sec;}
+	// format the output
+	var month = datetime.getMonth() + 1,
+			day = datetime.getDate(),
+			year = datetime.getFullYear(),
+			hour = datetime.getHours();
+	if (hour < 10) {
+	 hour = "0" + hour;
+	}
+	var min = datetime.getMinutes();
+	if (min < 10) {
+	 min = "0" + min;
+	}
+	var sec = datetime.getSeconds();
+	if (sec < 10) {
+	 sec = "0" + sec;
+	}
+	// put it all together
+	var dateTimeString = year + "-" + month + "-" + day +
+	" " + hour + ":" + min + ":" + sec;
+	return dateTimeString;
+ };
 
-  // put it all together
-  var dateTimeString = year + "-" + month + "-" +
-   day + " " + hour + ":" + min + ":" + sec;
-  return  dateTimeString;
-};
-module.exports =date;
+ module.exports = date;
 })();
