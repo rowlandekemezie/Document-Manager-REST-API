4c3382945b463845a68cd059ea091f0a528736ad
andela-rekemezie <rowland.ekemezie@andela.com>
2015-12-17 09:33:43 +0100

    feat(userAccess): user authorization
    -add middleware to authorize user role privileges
    -authorize document owner, SuperAdmin, or Documentarian to update/delete document

--- app/middlewares/userAccess.js
+++ app/middlewares/userAccess.js
@@ -3,9 +3,15 @@
 
  var Document = require('./../models/document.model'),
   config = require('./../../config/admin');
+ /**
+  * [userAccess middleware]
+  * @param  {[http request]}   req  [http request params and body]
+  * @param  {[http response]}   res  [response based on request]
+  * @param  {[control transfer]} next [description]
+  * @return {[access privilege]}        [Json response]
+  */
  module.exports = function(req, res, next) {
   Document.findById(req.params.id, function(err, doc) {
-    //console.log(doc);
    if (err) {
     res.status(500).send(err);
    } else {
@@ -17,7 +23,8 @@
     } else {
 
      if (req.decoded._id !== doc.ownerId &&
-      req.decoded.role !== config.role && req.decoded.role !== 'Documentarian') {
+      req.decoded.role !== config.role &&
+      req.decoded.role !== 'Documentarian') {
       res.status(401).json({
        success: false,
        message: 'Not authorized'
