8ce96eee2fa32b9b3c4a654c41962d4fc3b07a40
andela-rekemezie <rowland.ekemezie@andela.com>
2015-12-19 19:34:32 +0100

    refactor(routes): standardize endpoint definitions
    -change the roles/:userName endpoint to roles/
    
    -implement two spaces indentation across routes/

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,20 @@
 (function() {
- 'use strict';
+  'use strict';
 
- var userRoute = require('./user.route'),
-  documentRoute = require('./document.route'),
-  roleRoute = require('./role.route'),
+  var userRoute = require('./user.route'),
+    documentRoute = require('./document.route'),
+    roleRoute = require('./role.route'),
 
- /**
-  * [defineRoute]
-  * @param  {[FUNCTION]} router [Express router instance]
-  * @return {[JSON]}        [returns Json Object]
-  */
-  defineRoutes = function(router) {
-  userRoute(router);
-  documentRoute(router);
-  roleRoute(router);
- };
+    /**
+     * [defineRoute]
+     * @param  {[FUNCTION]} router [Express router instance]
+     * @return {[JSON]}        [returns Json Object]
+     */
+    defineRoutes = function(router) {
+      userRoute(router);
+      documentRoute(router);
+      roleRoute(router);
+    };
 
- module.exports = defineRoutes;
+  module.exports = defineRoutes;
 })();
--- app/routes/role.route.js
+++ app/routes/role.route.js
@@ -1,22 +1,22 @@
 (function() {
- 'use strict';
+  'use strict';
 
- var roleController = require('./../controllers/role.controller'),
-  userAuth = require('./../middlewares/userAuth'),
-  roleAuth = require('./../middlewares/roleAuth'),
-  verifyAdmin = require('./../middlewares/verifyAdmin');
+  var roleController = require('./../controllers/role.controller'),
+    userAuth = require('./../middlewares/userAuth'),
+    roleAuth = require('./../middlewares/roleAuth'),
+    verifyAdmin = require('./../middlewares/verifyAdmin');
 
- module.exports = function(router) {
+  module.exports = function(router) {
 
-  // allow admin to create and read roles
-  router.route('/roles/:userName')
-  	.post(userAuth, verifyAdmin, roleController.createRole)
-  	.get(userAuth, verifyAdmin, roleController.getAllRoles);
+    // allow admin to create and read roles
+    router.route('/roles')
+      .post(userAuth, verifyAdmin, roleController.createRole)
+      .get(userAuth, verifyAdmin, roleController.getAllRoles);
 
-  // allow authorized role to update or delete a particular role
-  router.route('/roles/update/:id')
-  	.get(userAuth, roleAuth, roleController.getRoleById)
-  	.put(userAuth, roleAuth, roleController.updateRole)
-  	.delete(userAuth, roleAuth, roleController.deleteRole);
- };
+    // allow authorized role to update or delete a particular role
+    router.route('/roles/:id')
+      .get(userAuth, roleAuth, roleController.getRoleById)
+      .put(userAuth, roleAuth, roleController.updateRole)
+      .delete(userAuth, roleAuth, roleController.deleteRole);
+  };
 })();
