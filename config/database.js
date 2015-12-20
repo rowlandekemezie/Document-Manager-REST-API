2a6e7bdd5b2ed5601beffe310a87fa3585f70b84
andela-rekemezie <rowland.ekemezie@andela.com>
2015-12-20 18:51:30 +0100

    chore(database): add database configurations

--- config/database.js
+++ config/database.js
@@ -1,7 +1,20 @@
 (function() {
   'use strict';
 
+  var env = process.env.NODE_ENV || 'development';
+  if (env === 'development') {
+    require('dotenv').load();
+  }
+
+  var envVariables = {
+    port: process.env.DATABASE_PORT,
+    url: process.env.DATABASE_URL,
+    secretKey: process.env.SECRET_KEY
+  };
+
   module.exports = {
-    db: 'mongodb://localhost/REST-API'
+    development: envVariables,
+    test: envVariables,
+    production: envVariables
   };
 })();
