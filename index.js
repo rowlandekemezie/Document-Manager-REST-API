7acc99a542fbabe5d69ba3c5733d781c1b939503
andela-rekemezie <rowland.ekemezie@andela.com>
2015-12-21 00:16:45 +0100

    refactor(index.js): rename server.js to index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+(function() {
+ 'use strict';
+
+  var env = process.env.NODE_ENV || 'development';
+  if (env === 'development') {
+    require('dotenv').load();
+  }
+
+  var database = require('./config/database')[env],
+    mongoose = require('mongoose'),
+    app = require('./config/express'),
+ 	port = process.env.PORT || 5555;
+
+  mongoose.connect(database.url);
+
+ app.listen(port, function() {
+  console.log('listening on port ' + port);
+ });
+
+ // expose the  server to app
+ module.exports = app;
+})();
