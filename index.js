816c3e183c64147ce24b39ec816bb112c4fc528e
andela-rekemezie <rowland.ekemezie@andela.com>
2015-12-21 02:34:29 +0100

    fix(index): indent code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function() {
- 'use strict';
+  'use strict';
 
   var env = process.env.NODE_ENV || 'development';
   if (env === 'development') {
@@ -9,14 +9,14 @@
   var database = require('./config/database')[env],
     mongoose = require('mongoose'),
     app = require('./config/express'),
- 	port = process.env.PORT || 5555;
+    port = process.env.PORT || 5555;
 
   mongoose.connect(database.url);
 
- app.listen(port, function() {
-  console.log('listening on port ' + port);
- });
+  app.listen(port, function() {
+    console.log('listening on port ' + port);
+  });
 
- // expose the  server to app
- module.exports = app;
+  // expose the  server to app
+  module.exports = app;
 })();
